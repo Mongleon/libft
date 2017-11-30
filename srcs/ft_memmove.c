d7ae6a3936957184394c3bd597ce849276097476
Valentin RICHARD <varichar@e2r5p20.42.fr>
2017-11-30 19:35:10 +0100

    fix ft_memmove.c

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: varichar <varichar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/06 15:15:26 by varichar          #+#    #+#             */
-/*   Updated: 2016/08/07 01:28:54 by varichar         ###   ########.fr       */
+/*   Updated: 2017/11/30 19:30:32 by varichar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,10 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
-	void	*tmp;
-
-	i = 0;
-	tmp = (void*)malloc(n);
-	if (tmp)
-	{
-		while (i < n)
-		{
-			((char*)tmp)[i] = ((char*)src)[i];
-			i++;
-		}
-		i = 0;
-		while (i < n)
-		{
-			((char*)dest)[i] = ((char*)tmp)[i];
-			i++;
-		}
-		free(tmp);
-	}
+	if (dest < src)
+		ft_memcpy(dest, src, n);
+	else
+		while (n-- > 0)
+			((char*)dest)[n] = ((char*)src)[n];
 	return (dest);
 }
